f99bcc1e659ee41ff7b4220ac2da9e4d7bb90649
Davi Wiliam <daviwiliam@protonmail.com>
2021-11-30 09:29:50 -0300

    first commit

---
+++ etapas.js
@@ -0,0 +1,69 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Princesa Leia',
+                partido: 'JEDAI',
+                foto:[
+                    {url:'leia.png', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '77722',
+                nome: 'C3PO',
+                partido: 'REBELDE',
+                foto:[
+                    {url:'C3PO.png', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '51666',
+                nome: 'Dart Maul',
+                partido: 'SITH',
+                foto:[
+                    {url:'darth_maul.png', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '38',
+                nome: 'Yoda',
+                partido: 'JEDAI',
+                vice: 'Luke Skywalker',
+                foto:[
+                    {url:'yoda.png', legenda: 'Prefeito'},
+                    {url:'luke.png', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '77',
+                nome: 'Han Solo',
+                partido: 'REBELDE',
+                vice: 'Chewbacca',
+                foto:[
+                    {url:'han_solo.png', legenda: 'Prefeito'},
+                    {url:'chewbacca.png', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '51',
+                nome: 'Darth Sidious',
+                partido: 'JEDAI',
+                vice: 'Darth Vader',
+                foto:[
+                    {url:'darth_sidious.png', legenda: 'Prefeito'},
+                    {url:'darth_vader.png', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+
+        ]
+    }
+]
